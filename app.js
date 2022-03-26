5130bcbf844c01afdd60bf4543869ec6e6f4e20f
faus34 <faustinogm1997@gmail.com>
2022-03-26 16:16:49 -0600

    layout comments

---
+++ app.js
@@ -0,0 +1,26 @@
+document.addEventListener(DOMContentLoaded, () => { //ejecuta el Script cuando el navegador HTML está completamente cargado y el árbol DOM está construido
+
+    const container = document.querySelector('.container');
+    const scoreDisplay = document.getElementById('score');
+    const width = 27; // Ancho del board 27 x 27 cuadros de 20px cada uno = 729 cuadros
+
+    const layout = []; //Layout numérico del board
+
+    //0 ---> Espacio vacio (clase= vacio)
+    //1 ---> Pared (clase=pared)
+    //2 ---> Migaja (clase= miga)
+    //3 ---> Queso (Clase= queso)
+
+        
+
+
+
+
+
+
+
+
+
+
+
+})
