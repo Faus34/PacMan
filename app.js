7148c1e767ea4155e16109d8059efccff0764cce
faus34 <faustinogm1997@gmail.com>
2022-03-26 12:19:18 -0600

    Elementos container y Score traidos a app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+document.addEventListener(DOMContentLoaded, () => { //ejecuta el Script cuando el navegador HTML está completamente cargado y el árbol DOM está construido
+
+    const container = document.querySelector('.container');
+    const scoreDisplay = document.getElementById('score');
+    const width = 27; // Ancho del board 27 x 27 cuadros de 20px cada uno = 729 cuadros
+
+    const layout = []; //Layout numérico del board
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
