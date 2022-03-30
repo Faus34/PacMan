2d219fa2cf61b31075c6d723351d7b411126c4fb
faus34 <faustinogm1997@gmail.com>
2022-03-29 20:53:46 -0600

    Quesos y fantasmas agregados al tablero

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
     const container = document.querySelector('.container');
     const scoreDisplay = document.getElementById('score');
     const width = 27; // Ancho del board 27 x 27 cuadros de 20px cada uno = 729 cuadros
@@ -5,18 +6,18 @@
     const layout =    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
                       ,1,2,2,2,2,2,2,2,2,2,2,2,2,1,2,2,2,2,2,2,2,2,2,2,2,2,1
                       ,1,2,1,1,1,1,2,1,1,1,1,1,2,1,2,1,1,1,1,1,2,1,1,1,1,2,1
-                      ,1,2,1,1,1,1,2,1,1,1,1,1,2,1,2,1,1,1,1,1,2,1,1,1,1,2,1
+                      ,1,3,1,1,1,1,2,1,1,1,1,1,2,1,2,1,1,1,1,1,2,1,1,1,1,3,1
                       ,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1
                       ,1,2,1,1,1,1,2,1,2,1,1,1,1,2,1,1,1,1,2,1,2,1,1,1,1,2,1
                       ,1,2,2,2,2,2,2,1,2,1,1,1,1,2,1,1,1,1,2,1,2,2,2,2,2,2,1
                       ,1,1,1,1,1,1,2,1,2,2,2,1,1,2,1,1,2,2,2,1,2,1,1,1,1,1,1
                       ,0,0,0,0,0,1,2,1,1,1,2,1,1,2,1,1,2,1,1,1,2,1,0,0,0,0,0
                       ,0,0,0,0,0,1,2,1,1,1,2,1,1,2,1,1,2,1,1,1,2,1,0,0,0,0,0
-                      ,0,0,0,0,0,1,2,1,0,0,0,0,0,0,0,0,0,0,0,1,2,1,0,0,0,0,0
-                      ,0,0,0,0,0,1,2,1,0,1,1,1,1,0,1,1,1,1,0,1,2,1,0,0,0,0,0
-                      ,1,1,1,1,1,1,2,1,0,1,0,0,0,0,0,0,0,1,0,1,2,1,1,1,1,1,1
-                      ,0,0,2,2,2,2,2,2,0,1,0,0,0,0,0,0,0,1,0,0,2,2,2,2,2,0,0
-                      ,1,1,1,1,1,1,2,1,0,1,0,0,0,0,0,0,0,1,0,1,2,1,1,1,1,1,1
+                      ,0,0,0,0,0,1,2,1,0,0,0,0,0,5,0,0,0,0,0,1,2,1,0,0,0,0,0
+                      ,0,0,0,0,0,1,2,1,0,1,1,1,1,4,1,1,1,1,0,1,2,1,0,0,0,0,0
+                      ,1,1,1,1,1,1,2,1,0,1,4,4,4,4,4,4,4,1,0,1,2,1,1,1,1,1,1
+                      ,0,0,2,2,2,2,2,2,0,1,4,5,4,5,4,5,4,1,0,2,2,2,2,2,2,0,0
+                      ,1,1,1,1,1,1,2,1,0,1,4,4,4,4,4,4,4,1,0,1,2,1,1,1,1,1,1
                       ,0,0,0,0,0,1,2,1,0,1,1,1,1,1,1,1,1,1,0,1,2,1,0,0,0,0,0
                       ,0,0,0,0,0,1,2,1,0,0,0,0,0,0,0,0,0,0,0,1,2,1,0,0,0,0,0
                       ,0,0,0,0,0,1,2,1,2,1,1,1,1,2,1,1,1,1,2,1,2,1,0,0,0,0,0
@@ -24,7 +25,7 @@
                       ,1,1,1,1,1,1,2,1,1,1,1,2,1,2,1,2,1,1,1,1,2,1,1,1,1,1,1
                       ,1,2,2,2,2,2,2,1,1,1,1,2,1,2,1,2,1,1,1,1,2,2,2,2,2,2,1
                       ,1,2,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,2,1
-                      ,1,2,2,2,2,1,2,1,1,1,1,1,1,2,1,1,1,1,1,1,2,1,2,2,2,2,1
+                      ,1,3,2,2,2,1,2,1,1,1,1,1,1,2,1,1,1,1,1,1,2,1,2,2,2,3,1
                       ,1,2,1,1,2,1,2,1,2,2,2,2,2,2,2,2,2,2,2,1,2,1,2,1,1,2,1
                       ,1,2,1,1,2,1,2,1,2,1,1,1,1,1,1,1,1,1,2,1,2,1,2,1,1,2,1
                       ,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1
@@ -34,7 +35,8 @@
     //2 ---> Migaja (clase= miga)
     //3 ---> Queso (Clase= queso)
     //4 ---> Casa de fantasmas (clase = casa-fantasma)
-
+    //5 ---> Fantasma (default=pinky) clase = fantasma
+    
     let celdas = [];
     //Función para crear divs dentro del contendor del HTML y asignarles clases según su número en el layout
     function createBoard() {
@@ -52,11 +54,18 @@
                     celdas[i].classList.add('pared');
                 break;
                 case 2: 
-                    celdas[i].classList.add('miga-back');
+                    celdas[i].classList.add('subElement-back');
                     celdas[i].appendChild(subElemento).classList.add('miga');
                 break;
                 case 3:
-                    celdas[i].classList.add('queso');
+                    celdas[i].classList.add('subElement-back');
+                    celdas[i].appendChild(subElemento).classList.add('queso');
+                break;
+                case 4:
+                    celdas[i].classList.add('casa-fantasma');
+                break;
+                case 5:
+                    celdas[i].classList.add('fantasma');
                 break;
                 default:
                     console.log('createBoard() error, unknown class');
@@ -66,7 +75,27 @@
     }
 
     createBoard();
-    console.log(celdas);
+
+    class Ghost { //Clase prototipo de fantasmas startIndex es su posicion en layout
+        constructor(className,startIndex,speed){
+            this.className = className;
+            this.startIndex = startIndex;
+            this.speed = speed;
+            this.currentIndex = startIndex;
+            this.timerId = NaN;
+        }
+    };
+
+    let ghosts = [
+        new Ghost('blue',362,250),
+        new Ghost('pinky',364,400),
+        new Ghost('red',366,300),
+        new Ghost('orange',283,500)
+    ];
+
+    ghosts.forEach(ghost => { //funcion para agregar className de cada tipo de fantasma a cuadricula
+        celdas[ghost.currentIndex].classList.add(ghost.className); //al div que se encuentre en el currentIndex del fantasma se le asigna una clase adicional segun el className de cada tipo de fantasma.
+    });
 
 
 
