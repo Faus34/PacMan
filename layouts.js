bb15762f1a35313cb5a4f59aa3f77567d03404ba
faus34 <faustinogm1997@gmail.com>
2022-04-13 17:31:40 -0500

    volumen adjustment

--- layouts.js
+++ layouts.js
@@ -62,10 +62,10 @@ let powerEatenSound = new Audio();
 powerEatenSound.src = 'https://firebasestorage.googleapis.com/v0/b/pacman-19a45.appspot.com/o/SoundEffects%2Fpacman_eatfruit.mp3?alt=media&token=d80b090b-d79e-405f-813b-db535f8bb507';
 let yeyy = new Audio();
 yeyy.src = 'https://firebasestorage.googleapis.com/v0/b/pacman-19a45.appspot.com/o/SoundEffects%2FYayyy.mp3?alt=media&token=e2196577-b884-474a-bf71-05dcf076a1b9';
-yeyy.volume = 0.1;
+yeyy.volume = 0.3;
 let cricket = new Audio();
 cricket.src = 'https://www.myinstants.com/media/sounds/awkward-cricket-sound-effect.mp3';
-cricket.volume = 0.1;
+cricket.volume = 0.3;
 
 let popupContent = [
     'Luz, cuando estaba haciendo este juego no paraba de pensar que soy muy afortunado, porque últimamente descubrí que estoy seguro de tres cosas. La primera es que me apasiona lo que hago (aunque aun tenga mucho por aprender).',
